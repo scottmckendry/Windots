e9c449f6f4e02d27d8741d2c618f32ae96cedb6b
Scott McKendry <scott.c.mckendry@gmail.com>
2023-08-05 17:45:53 +1200

    Started setting up Lazyvim config

--- nvim/lua/custom/options.lua
+++
@@ -1,7 +0,0 @@
--- Set shell to PowerShell 7
-vim.opt.shell = "pwsh"
-vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
-vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-vim.opt.shellquote = ""
-vim.opt.shellxquote = ""
