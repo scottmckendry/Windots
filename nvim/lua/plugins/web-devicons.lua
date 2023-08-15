86c357414316e0e1e8f4fb0cf21dbe4bf843f62d
scottmckendry <me@scottmckendry.tech>
2023-08-15 19:53:47 +1200

    Add custom icons for yml and go

--- nvim/lua/plugins/web-devicons.lua
+++ nvim/lua/plugins/web-devicons.lua
@@ -6,6 +6,18 @@ local M = {
       require("nvim-web-devicons").setup({
         default = true,
         color_icons = true,
+        override = {
+          yaml = {
+            icon = "",
+            color = "#f38ba8",
+            name = "Yaml"
+          },
+          go = {
+            icon = "󰟓",
+            color = "#79d4fd",
+            name = "Go"
+          },
+        },
         override_by_extension = {
           ["bicep"] = {
             icon = "",
