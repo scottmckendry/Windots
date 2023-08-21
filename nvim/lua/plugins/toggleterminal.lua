f41e2feefc74b77cd90251eabf4ea379f2fc6e9c
Scott McKendry <me@scottmckendry.tech>
2023-08-21 21:11:31 +1200

    Enable lazy loading for custom plugins

--- nvim/lua/plugins/toggleterminal.lua
+++ nvim/lua/plugins/toggleterminal.lua
@@ -2,27 +2,13 @@
 local M = {
   {
     "akinsho/toggleterm.nvim",
-    config = function()
-      require("toggleterm").setup({
-        -- size can be a number or function which is passed the current terminal
-        size = function(term)
-          if term.direction == "horizontal" then
-            return 15
-          elseif term.direction == "vertical" then
-            return vim.o.columns * 0.4
-          end
-        end,
-        open_mapping = [[<c-\>]],
-        hide_numbers = true, -- hide the number column in toggleterm buffers
-        shade_filetypes = {},
-        shade_terminals = true,
-        shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-        start_in_insert = true,
-        insert_mappings = true, -- whether or not the open mapping applies in insert mode
-        persist_size = true,
-        direction = "horizontal",
-      })
-    end,
+    keys = {
+      { "<C-\\>", "<cmd>ToggleTerm size=20<cr>", desc = "Toggle Terminal", mode = "n" },
+      { "<C-\\>", "<cmd>ToggleTerm<cr>",         desc = "Toggle Terminal", mode = "t" },
+    },
+    opts = {
+      shade_terminals = false,
+    },
   },
 }
 
