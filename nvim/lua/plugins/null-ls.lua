a85c85513bd81774976957e2d1bde74756ed1153
Scott McKendry <me@scottmckendry.tech>
2023-08-21 21:11:49 +1200

    Fix null-ls timeouts in Go

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,6 +1,5 @@
 -- null-ls https://github.com/jose-elias-alvarez/null-ls.nvim
 local null_ls = require("null-ls")
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local M = {
   {
     "jose-elias-alvarez/null-ls.nvim",
@@ -11,24 +10,6 @@ local M = {
           null_ls.builtins.formatting.goimports_reviser,
           null_ls.builtins.formatting.golines,
         },
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({
-              group = augroup,
-              buffer = bufnr,
-            })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({
-                  bufnr = bufnr,
-                  timeout_ms = 4000,
-                })
-              end,
-            })
-          end
-        end,
       }
     end,
   },
