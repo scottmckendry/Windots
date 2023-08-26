500dfe9e9feb0668e926d471b70b90eb2fb13a65
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-08-26 18:06:36 +1200

    Remove noice config, add go tools to mason

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -7,6 +7,8 @@ local M = {
       ensure_installed = {
         "json-lsp",
         "gopls",
+        "goimports-reviser",
+        "golines",
         "bicep-lsp",
         "dockerfile-language-server",
         "docker-compose-language-service"
