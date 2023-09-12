e8b21964d4a2506fabadce9aaad0f6a31d38d1f6
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-09-12 22:07:55 +1200

    Hide .git from nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -41,7 +41,10 @@ return {
         },
       },
       filters = {
-        custom = { ".DS_Store" },
+        custom = { 
+          ".DS_Store",
+          ".git",
+        },
       },
       git = {
         ignore = false,
