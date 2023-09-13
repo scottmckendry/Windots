082ac30bbe96ffc00589dde331c0c556a2d45b57
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-09-13 00:34:19 +0000

    Add conditional logic for linux-specific settings [skip ci] Automated sync from scottmckendry/dots

--- nvim/lua/plugins/colourscheme.lua
+++ nvim/lua/plugins/colourscheme.lua
@@ -4,7 +4,16 @@ return {
 		priority = 1000,
 		lazy = false,
 		config = function()
-			vim.cmd [[colorscheme tokyonight-night]]
+			if vim.fn.has("unix") == 1 then
+				require("tokyonight").setup({
+					transparent = true,
+					styles = {
+						sidebars = "transparent",
+						floats = "transparent",
+					},
+				})
+			end
+			vim.cmd([[colorscheme tokyonight-night]])
 		end,
 	},
 }
