8cb58c5fd5c769604c34d020b08496e0e857206b
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-09-13 00:12:30 +0000

    Add .gitignore & .gitconfig [skip ci] Automated sync from scottmckendry/dots

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,54 +1,58 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  cmd = { "NvimTreeToggle" },
-  keys = {
-    { "<leader>ee", ":NvimTreeToggle<CR>",         desc = "Toggle file explorer",                 silent = true },
-    { "<leader>ef", ":NvimTreeFindFileToggle<CR>", desc = "Toggle file explorer on current file", silent = true },
-    { "<leader>ec", ":NvimTreeCollapse<CR>",       desc = "Collapse file explorer",               silent = true },
-    { "<leader>er", ":NvimTreeRefresh<CR>",        desc = "Refresh file explorer",                silent = true },
-  },
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local nvimtree = require("nvim-tree")
+	"nvim-tree/nvim-tree.lua",
+	cmd = { "NvimTreeToggle" },
+	keys = {
+		{ "<leader>ee", ":NvimTreeToggle<CR>", desc = "Toggle file explorer", silent = true },
+		{ "<leader>ef", ":NvimTreeFindFileToggle<CR>", desc = "Toggle file explorer on current file", silent = true },
+		{ "<leader>ec", ":NvimTreeCollapse<CR>", desc = "Collapse file explorer", silent = true },
+		{ "<leader>er", ":NvimTreeRefresh<CR>", desc = "Refresh file explorer", silent = true },
+	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local nvimtree = require("nvim-tree")
 
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+		vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 
-    nvimtree.setup({
-      view = {
-        width = 35,
-      },
-      renderer = {
-        indent_markers = {
-          enable = true
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "",
-              arrow_open = "",
-            },
-          },
-        },
-      },
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      filters = {
-        custom = { 
-          ".DS_Store",
-          ".git",
-        },
-      },
-      git = {
-        ignore = false,
-      },
-    })
-  end,
+		nvimtree.setup({
+			view = {
+				width = 35,
+			},
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "",
+							arrow_open = "",
+						},
+					},
+				},
+			},
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			filters = {
+				custom = {
+					".DS_Store",
+					".git",
+				},
+				ignore = {
+					".gitignore",
+					".gitconfig",
+				},
+			},
+			git = {
+				ignore = false,
+			},
+		})
+	end,
 }
