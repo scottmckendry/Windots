6ec0ea03b0676e732beffe17e94952a9e1a1b471
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-10-05 21:36:50 +0000

    Fix Notify error for transparent themes [skip ci] Automated sync from scottmckendry/dots

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -89,4 +89,14 @@ return {
             },
         },
     },
+    {
+        "rcarriga/nvim-notify",
+        event = "VeryLazy",
+        config = function()
+            require("notify").setup {
+                background_colour = "#000000",
+            }
+        end,
+
+    }
 }
