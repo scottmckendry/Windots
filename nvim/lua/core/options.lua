f7830afb4a71220b87c5edfe7e1fa4513409ca9d
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-10-08 11:41:03 +1300

    Fix: use spaces instead of tabs in stylua :white_check_mark:

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -7,7 +7,7 @@ local opt = vim.opt
 if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
     opt.shell = "pwsh -NoLogo"
     opt.shellcmdflag =
-    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+        "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
     opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
     opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
     opt.shellquote = ""
@@ -24,7 +24,8 @@ opt.termguicolors = true
 opt.confirm = true
 opt.mouse = "a"
 opt.undofile = true
-opt.fillchars:append { eob = " " }
+opt.fillchars:append({ eob = " " })
+opt.swapfile = false
 
 -- Set tab width
 opt.tabstop = 4
--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,26 +1,30 @@
 return {
-	"stevearc/conform.nvim",
-	event = "BufReadPre",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				go = { "goimports_reviser", "gofmt", "golines" },
-				lua = { "stylua" },
-				html = { "prettier" },
-				javascript = { "prettier" },
-				css = { "prettier" },
-			},
+    "stevearc/conform.nvim",
+    event = "BufReadPre",
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                go = { "goimports_reviser", "gofmt", "golines" },
+                lua = { "stylua" },
+                html = { "prettier" },
+                javascript = { "prettier" },
+                css = { "prettier" },
+            },
 
-			format_after_save = {
-				lsp_fallback = true,
-			},
+            format_after_save = {
+                lsp_fallback = true,
+            },
 
-			formatters = {
-				goimports_reviser = {
-					command = "goimports-reviser",
-					args = { "-output", "stdout", "$FILENAME" },
-				},
-			},
-		})
-	end,
+            formatters = {
+                goimports_reviser = {
+                    command = "goimports-reviser",
+                    args = { "-output", "stdout", "$FILENAME" },
+                },
+            },
+        })
+
+        -- Override stylua's default indent type
+        table.insert(require("conform.formatters.stylua").args, "--indent-type")
+        table.insert(require("conform.formatters.stylua").args, "Spaces")
+    end,
 }
