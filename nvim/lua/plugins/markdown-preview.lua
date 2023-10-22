772eb518601b5203051d0d680f13c472ec2f7c99
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-10-22 21:21:43 +1300

    simplify markdown-preview build :hammer:

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,35 +1,13 @@
 return {
     "iamcco/markdown-preview.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
+    ft = { "markdown" },
     cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
+    build = function()
+        local install_path = vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim/app"
+        vim.cmd("silent !cd " .. install_path .. " && npm install && git restore .")
+    end,
     init = function()
         vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-    config = function()
-        local job = require("plenary.job")
-        local install_path = vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim/app"
-        local cmd = "bash"
-
-        if vim.fn.has("win64") == 1 then
-            cmd = "pwsh"
-        end
-
-        job:new({
-            command = cmd,
-            args = { "-c", "npm install && git restore ." },
-            cwd = install_path,
-            on_exit = function()
-                print("Finished installing markdown-preview.nvim")
-            end,
-            on_stderr = function(_, data)
-                print(data)
-            end,
-        }):start()
-
-        -- Options
         vim.g.mkdp_auto_close = 0
     end,
 }
