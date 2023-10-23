b5469a65fe7c8467907d74fe26d7612f7e8ecf78
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-10-24 11:47:16 +1300

    Add conform formatter config for pwsh :tada:

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,6 +2,14 @@ return {
     "stevearc/conform.nvim",
     event = "BufReadPre",
     config = function()
+        local lsp_fallback = setmetatable({
+            bicep = "always",
+        }, {
+            -- default true
+            __index = function()
+                return true
+            end,
+        })
         require("conform").setup({
             formatters_by_ft = {
                 go = { "goimports_reviser", "gofmt", "golines" },
@@ -10,16 +18,33 @@ return {
                 javascript = { "prettier" },
                 css = { "prettier" },
                 json = { "prettier" },
+                ps1 = { "powershell", "trim_whitespace", "trim_newlines" },
+                bicep = { "trim_whitespace", "trim_newlines" },
             },
 
-            format_after_save = {
-                lsp_fallback = true,
-            },
+            format_after_save = function(buf)
+                return {
+                    lsp_fallback = lsp_fallback[vim.bo[buf].filetype],
+                }
+            end,
 
             formatters = {
                 goimports_reviser = {
                     command = "goimports-reviser",
                     args = { "-output", "stdout", "$FILENAME" },
+                },
+                powershell = {
+                    command = "pwsh",
+                    args = {
+                        "-NoLogo",
+                        "-NoProfile",
+                        "-NonInteractive",
+                        "-Command",
+                        "Invoke-Formatter",
+                        "( Get-Content -Raw -Path",
+                        "$FILENAME",
+                        ")",
+                    },
                 },
             },
         })
