6fdbf3d338c570f233619caeceb3c4afb0c8e71f
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-12-25 08:23:05 +1300

    Remove highlights from cmp config :coffin:

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -21,9 +21,8 @@ return {
 
         cmp.setup({
             window = {
-                completion = cmp.config.window.bordered({
-                    winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
-                }),
+                completion = cmp.config.window.bordered({}),
+                documentation = cmp.config.window.bordered({}),
                 scrollbar = false,
             },
             completion = {
@@ -73,23 +72,5 @@ return {
                 { name = "cmdline", max_item_count = max_items },
             }),
         })
-
-        -- set highlights for completion types
-        -- grey
-        vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { bg = "NONE", strikethrough = true, fg = "#808080" })
-        -- blue
-        vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { bg = "NONE", fg = "#569CD6" })
-        vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { link = "CmpIntemAbbrMatch" })
-        -- light blue
-        vim.api.nvim_set_hl(0, "CmpItemKindVariable", { bg = "NONE", fg = "#9CDCFE" })
-        vim.api.nvim_set_hl(0, "CmpItemKindInterface", { link = "CmpItemKindVariable" })
-        vim.api.nvim_set_hl(0, "CmpItemKindText", { link = "CmpItemKindVariable" })
-        -- pink
-        vim.api.nvim_set_hl(0, "CmpItemKindFunction", { bg = "NONE", fg = "#C586C0" })
-        vim.api.nvim_set_hl(0, "CmpItemKindMethod", { link = "CmpItemKindFunction" })
-        -- front
-        vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { bg = "NONE", fg = "#D4D4D4" })
-        vim.api.nvim_set_hl(0, "CmpItemKindProperty", { link = "CmpItemKindKeyword" })
-        vim.api.nvim_set_hl(0, "CmpItemKindUnit", { link = "CmpItemKindKeyword" })
     end,
 }
