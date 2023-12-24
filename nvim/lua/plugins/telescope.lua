5dc1d968c4f4fcdcb392bcd4b123efc25299bf65
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-12-24 13:52:10 +1300

    Move telescope theming to cyberdream :rocket:

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -9,41 +9,6 @@ return {
         local telescope = require("telescope")
         local actions = require("telescope.actions")
 
-        -- Get current theme colours
-        local get_hlgroup = require("core.utils").get_hlgroup
-        local normal = get_hlgroup("Normal")
-        local fg, bg = normal.fg, normal.bg
-        local bg_alt = get_hlgroup("Visual").bg
-        local green = get_hlgroup("String").fg
-        local red = get_hlgroup("Error").fg
-
-        -- Set solid background if transparency is enabled
-        if bg == "NONE" then
-            bg = "#1e222a"
-            bg_alt = "#2a2e36"
-        end
-
-        -- Create table of colours for telescope for pretty borderless theme
-        local telescope_colours = {
-            TelescopeBorder = { fg = bg_alt, bg = bg },
-            TelescopeNormal = { bg = bg },
-            TelescopePreviewBorder = { fg = bg, bg = bg },
-            TelescopePreviewNormal = { bg = bg },
-            TelescopePreviewTitle = { fg = bg, bg = green },
-            TelescopePromptBorder = { fg = bg_alt, bg = bg_alt },
-            TelescopePromptNormal = { fg = fg, bg = bg_alt },
-            TelescopePromptPrefix = { fg = red, bg = bg_alt },
-            TelescopePromptTitle = { fg = bg, bg = red },
-            TelescopeResultsBorder = { fg = bg, bg = bg },
-            TelescopeResultsNormal = { bg = bg },
-            TelescopeResultsTitle = { fg = bg, bg = bg },
-        }
-
-        -- Apply colours to telescope
-        for hlgroup, colours in pairs(telescope_colours) do
-            vim.api.nvim_set_hl(0, hlgroup, colours)
-        end
-
         telescope.setup({
             pickers = {
                 colorscheme = {
@@ -51,10 +16,11 @@ return {
                 },
 
                 find_files = {
-                    hidden = true,
+                    file_ignore_patterns = { ".git%p" },
                 },
             },
             defaults = {
+                hidden = true,
                 prompt_prefix = "   ",
                 selection_caret = "  ",
                 entry_prefix = "  ",
@@ -82,11 +48,6 @@ return {
                         ["<C-j>"] = actions.move_selection_next, -- move to next result
                         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
                     },
-                },
-            },
-            pickers = {
-                find_files = {
-                    file_ignore_patterns = { ".git%p" },
                 },
             },
         })
