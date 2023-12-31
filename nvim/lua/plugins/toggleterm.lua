0ddd8bc8b332d49c9eef8180f0503b3d28aeff8f
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-01-01 11:47:29 +1300

    add dynamic window sizing to toggleterm :hammer:

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,13 +1,16 @@
+local size = function()
+    return vim.o.columns * 0.5
+end
+
 return {
     "akinsho/toggleterm.nvim",
     keys = {
-        { "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal", mode = "n" },
+        { "<C-\\>", "<cmd>ToggleTerm size=" .. size() .. "<cr>", desc = "Toggle Terminal", mode = "n" },
         { "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal", mode = "t" },
     },
     opts = {
         shade_terminals = true,
         direction = "vertical",
-        size = 80,
         float_opts = {
             -- Hide border
             border = "none",
