535dcada42091c0fa10792735cbdf0242dc2a6f7
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-04-06 13:36:55 +1300

    set section separator char for lualine :adhesive_bandage:

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -13,6 +13,7 @@ return {
         return {
             options = {
                 component_separators = { left = " ", right = " " },
+                section_separators = { left = " ", right = " " },
                 theme = cyberdream,
                 globalstatus = true,
                 disabled_filetypes = { statusline = { "dashboard", "alpha" } },
