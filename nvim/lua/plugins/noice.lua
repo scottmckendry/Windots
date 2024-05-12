052058c727713334eb64ada7bcc8991100d5c5ec
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-05-12 21:42:19 +1200

    remove unecessary nvim-notify config :recycle:

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -46,13 +46,4 @@ return {
             })
         end,
     },
-    {
-        "rcarriga/nvim-notify",
-        event = "VeryLazy",
-        config = function()
-            require("notify").setup({
-                background_colour = "#000000",
-            })
-        end,
-    },
 }
