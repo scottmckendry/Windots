1e9eb2537d5b70319710758b179372f4ae0d574a
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-06-08 16:14:16 +1200

    add copilot chat :speech_balloon:

---
+++ nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,75 @@
+return {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    cmd = "CopilotChat",
+    keys = {
+        { "<leader>a", "", desc = "+ai" },
+        {
+            "<leader>aa",
+            function()
+                return require("CopilotChat").toggle()
+            end,
+            desc = "Toggle (CopilotChat)",
+            mode = { "n", "v" },
+        },
+        {
+            "<leader>ax",
+            function()
+                return require("CopilotChat").reset()
+            end,
+            desc = "Clear (CopilotChat)",
+            mode = { "n", "v" },
+        },
+        {
+            "<leader>aq",
+            function()
+                local input = vim.fn.input("Quick Chat: ")
+                if input ~= "" then
+                    require("CopilotChat").ask(input)
+                end
+            end,
+            desc = "Quick Chat (CopilotChat)",
+            mode = { "n", "v" },
+        },
+        {
+            "<leader>ac",
+            ":CopilotChatCommit<cr>",
+            desc = "Commit (CopilotChat)",
+            mode = { "n", "v" },
+        },
+    },
+    config = function()
+        require("CopilotChat.integrations.cmp").setup()
+        vim.api.nvim_create_autocmd("BufEnter", {
+            pattern = "copilot-chat",
+            callback = function()
+                vim.opt_local.relativenumber = false
+                vim.opt_local.number = false
+            end,
+        })
+
+        require("CopilotChat").setup({
+            model = "gpt-4",
+            auto_insert_mode = true,
+            show_help = false,
+            show_folds = false,
+            question_header = "  Scott ",
+            answer_header = "  Copilot ",
+            window = {
+                layout = "float",
+                width = 0.6,
+                height = 0.7,
+            },
+            mappings = {
+                close = {
+                    normal = "q",
+                    insert = "C-q",
+                },
+            },
+            selection = function(source)
+                local select = require("CopilotChat.select")
+                return select.visual(source) or select.buffer(source)
+            end,
+        })
+    end,
+}
