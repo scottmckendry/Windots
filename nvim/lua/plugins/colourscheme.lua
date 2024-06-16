4ed31a1e2dda7bcad6477fa86a903d8a8bd298c5
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-06-17 07:57:27 +1200

    make use of new features in cyberdream v3.1 :lipstick:

--- nvim/lua/plugins/colourscheme.lua
+++ nvim/lua/plugins/colourscheme.lua
@@ -10,6 +10,19 @@ return {
                 italic_comments = true,
                 hide_fillchars = true,
                 terminal_colors = false,
+                borderless_telescope = { border = false, style = "nvchad" },
+                theme = {
+                    variant = "auto",
+                    overrides = function(colours)
+                        return {
+                            TelescopePromptPrefix = { fg = colours.blue },
+                            TelescopeMatching = { fg = colours.cyan },
+                            TelescopeResultsTitle = { fg = colours.blue },
+                            TelescopePromptCounter = { fg = colours.cyan },
+                            TelescopePromptTitle = { fg = colours.bg, bg = colours.blue, bold = true },
+                        }
+                    end,
+                },
             })
 
             vim.cmd("colorscheme cyberdream")
