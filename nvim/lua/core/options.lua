85632e3777022a427e81643331cbdee7aa4478f0
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-06-17 07:56:42 +1200

    remove duplicated cursorline option :coffin:

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -18,7 +18,7 @@ end
 opt.number = true
 opt.relativenumber = true
 opt.ignorecase = true
-opt.cursorline = true
+opt.cursorline = false
 opt.clipboard = "unnamedplus"
 opt.termguicolors = true
 opt.confirm = true
@@ -29,7 +29,6 @@ opt.conceallevel = 1
 opt.scrolloff = 12
 opt.wrap = true
 opt.linebreak = true
-opt.cursorline = false
 
 -- Set tab width
 opt.tabstop = 4
