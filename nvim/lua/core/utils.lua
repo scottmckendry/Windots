056d30e573de80b4f549005e688b7ff2407a8b75
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-06-19 06:31:28 +1200

    add buffer utility functions :hammer:

--- nvim/lua/core/utils.lua
+++ nvim/lua/core/utils.lua
@@ -1,9 +1,9 @@
 local M = {}
 
 --- Get highlight properties for a given highlight name
----@param name string The highlight group name
----@param fallback? table The fallback highlight properties
----@return table properties # the highlight group properties
+--- @param name string The highlight group name
+--- @param fallback? table The fallback highlight properties
+--- @return table properties # the highlight group properties
 function M.get_hlgroup(name, fallback)
     if vim.fn.hlexists(name) == 1 then
         local group = vim.api.nvim_get_hl(0, { name = name })
@@ -28,7 +28,30 @@ function M.delete_buffer(buf)
     vim.api.nvim_command("bwipeout " .. buf)
 end
 
+--- Switch to the previous buffer
+function M.switch_to_previous_buffer()
+    local ok, _ = pcall(function()
+        vim.cmd("buffer #")
+    end)
+    if not ok then
+        vim.notify("No other buffer to switch to!", 3, { title = "Warning" })
+    end
+end
+
+--- Get the number of open buffers
+--- @return number
+function M.get_buffer_count()
+    local count = 0
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.fn.bufname(buf) ~= "" then
+            count = count + 1
+        end
+    end
+    return count
+end
+
 --- Parse a given integer color to a hex value.
+--- @param int_color number
 function M.parse_hex(int_color)
     return string.format("#%x", int_color)
 end
