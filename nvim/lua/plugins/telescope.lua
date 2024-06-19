eedb8b60c50d4ce8edd0b97debc4842a7bd136c2
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-06-19 15:41:08 +1200

    multi-file support for telescope :bento:

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -9,13 +9,28 @@ return {
         local telescope = require("telescope")
         local actions = require("telescope.actions")
 
+        local select_one_or_multi = function(prompt_bufnr)
+            local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+            local multi = picker:get_multi_selection()
+            if not vim.tbl_isempty(multi) then
+                require("telescope.actions").close(prompt_bufnr)
+                for _, j in pairs(multi) do
+                    if j.path ~= nil then
+                        vim.cmd(string.format("%s %s", "edit", j.path))
+                    end
+                end
+            else
+                require("telescope.actions").select_default(prompt_bufnr)
+            end
+        end
+
         telescope.setup({
             pickers = {
                 find_files = {
                     find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
                 },
                 buffers = {
-                    theme = "cursor",
+                    theme = "dropdown",
                     previewer = false,
                     mappings = {
                         i = {
@@ -56,6 +71,7 @@ return {
                         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
                         ["<C-j>"] = actions.move_selection_next, -- move to next result
                         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                        ["<cr>"] = select_one_or_multi,
                     },
                 },
             },
