938547b0ced00fdcac1afa4b6ef35f2651ba1bd5
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-06-29 21:54:27 +1200

    update lazy.nvim bootstrap logic :building_construction:

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -1,14 +1,7 @@
--- Bootstrap Lazy Vim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
