0f4daab55b0d5270b5b6ce109b5e03b692f3943a
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-07-24 06:23:42 +1200

    configure fancy block tabs for wezterm :lipstick:

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -35,26 +35,37 @@ config.default_prog = { "pwsh", "-NoLogo" }
 -- Tabs
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
+config.show_tab_index_in_tab_bar = false
 config.use_fancy_tab_bar = false
 config.colors.tab_bar = {
     background = transparent_bg,
-    active_tab = {
-        bg_color = transparent_bg,
-        fg_color = "#ffffff",
-    },
-    inactive_tab = {
-        bg_color = transparent_bg,
-        fg_color = "#7b8496",
-    },
-    inactive_tab_hover = {
-        bg_color = transparent_bg,
-        fg_color = "#ffffff",
-    },
-    new_tab = {
-        bg_color = transparent_bg,
-        fg_color = "#ffffff",
-    },
+    new_tab = { fg_color = config.colors.background, bg_color = config.colors.brights[6] },
+    new_tab_hover = { fg_color = config.colors.background, bg_color = config.colors.foreground },
 }
+
+wezterm.on("format-tab-title", function(tab, _, _, _, hover)
+    local background = config.colors.brights[1]
+    local foreground = config.colors.foreground
+
+    if tab.is_active then
+        background = config.colors.brights[7]
+        foreground = config.colors.background
+    elseif hover then
+        background = config.colors.brights[8]
+        foreground = config.colors.background
+    end
+
+    local title = tostring(tab.tab_index + 1)
+    return {
+        { Foreground = { Color = background } },
+        { Text = "█" },
+        { Background = { Color = background } },
+        { Foreground = { Color = foreground } },
+        { Text = title },
+        { Foreground = { Color = background } },
+        { Text = "█" },
+    }
+end)
 
 -- Keybindings
 config.keys = {
