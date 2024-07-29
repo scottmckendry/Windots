f6e9e38450ea0500af5bd3fa6d7e10e19165bec3
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-07-29 19:06:04 +1200

    tidy up toggleterm.nvim config :stethoscope:

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,19 +1,12 @@
-local size = function()
-    return vim.o.columns * 0.5
-end
-
 return {
     "akinsho/toggleterm.nvim",
     keys = {
-        { "<C-\\>", "<cmd>ToggleTerm size=" .. size() .. "<cr>", desc = "Toggle Terminal", mode = "n" },
+        { "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal", mode = "n" },
         { "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal", mode = "t" },
     },
     opts = {
         shade_terminals = true,
         direction = "vertical",
-        float_opts = {
-            -- Hide border
-            border = "none",
-        },
+        size = vim.o.columns * 0.5,
     },
 }
