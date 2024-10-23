ef8ee0f17ce83d270ac29b88d71c05fee4b6aa66
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-10-23 21:28:49 +0000

    configure roslyn to support nix package :package: [skip ci] Automated sync from scottmckendry/nix

--- nvim/lua/plugins/roslyn.lua
+++ nvim/lua/plugins/roslyn.lua
@@ -3,5 +3,16 @@ return {
     ft = { "cs" },
     -- TODO: Add build function that handles roslyn install until Mason supports it:
     -- https://github.com/mason-org/mason-registry/pull/6330
-    opts = {},
+    config = function()
+        local exe = nil
+        if vim.fn.has("unix") == 1 then
+            local os = vim.fn.system("cat /etc/os-release | grep ^ID= | cut -d= -f2")
+            if string.match(os, "nixos") then
+                exe = { "Microsoft.CodeAnalysis.LanguageServer" }
+            end
+        end
+        require("roslyn").setup({
+            exe = exe,
+        })
+    end,
 }
