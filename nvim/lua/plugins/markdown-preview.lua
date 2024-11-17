c09dd3f692de65e8c3e9522f2aa480099c0f7495
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-11-17 03:13:16 +0000

    simplify md preview build cmd :wrench: [skip ci] Automated sync from scottmckendry/nix

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -2,10 +2,7 @@ return {
     "iamcco/markdown-preview.nvim",
     ft = { "markdown" },
     cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
-    build = function()
-        local install_path = vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim/app"
-        vim.cmd("silent !cd " .. install_path .. " && npm install && git restore .")
-    end,
+    build = "cd app && npm install && git reset --hard",
     init = function()
         vim.g.mkdp_filetypes = { "markdown" }
         vim.g.mkdp_auto_close = 0
