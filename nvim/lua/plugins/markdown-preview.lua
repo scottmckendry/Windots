12e48fe2d582cbb61ecef824b7b553ce9864526f
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-11-19 06:46:21 +1300

    fix markdown preview build cmd :adhesive_bandage:

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -2,7 +2,10 @@ return {
     "iamcco/markdown-preview.nvim",
     ft = { "markdown" },
     cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
-    build = "cd app && npm install && git reset --hard",
+    build = function()
+        local install_path = vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim/app"
+        vim.cmd("!cd " .. install_path .. " && npm install && git reset --hard")
+    end,
     init = function()
         vim.g.mkdp_filetypes = { "markdown" }
         vim.g.mkdp_auto_close = 0
