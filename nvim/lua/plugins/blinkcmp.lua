97167fdff06e69dc508fe9c5986e2ae3c50f5d09
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-11-19 18:02:49 +0000

    update blink.cmp config :bookmark: [skip ci] Automated sync from scottmckendry/nix

--- nvim/lua/plugins/blinkcmp.lua
+++ nvim/lua/plugins/blinkcmp.lua
@@ -1,18 +1,12 @@
 return {
     "saghen/blink.cmp",
-    dev = true,
     lazy = false,
     dependencies = "rafamadriz/friendly-snippets",
     build = "cargo build --release",
     config = function()
         require("blink.cmp").setup({
-            keymap = {
-                accept = "<C-y>",
-                select_next = "<C-n>",
-            },
             windows = {
                 autocomplete = {
-                    draw = "reversed",
                     border = {
                         { "󱐋", "WarningMsg" },
                         "─",
