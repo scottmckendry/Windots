b60da770b38a14d5957ebc8fd5bd0507f5544ff5
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-12-05 17:25:33 +0000

    update smear cursor config :gear: [skip ci] Automated sync from scottmckendry/nix

--- nvim/lua/plugins/smear-cursor.lua
+++ nvim/lua/plugins/smear-cursor.lua
@@ -7,11 +7,6 @@ return {
     "sphamba/smear-cursor.nvim",
     lazy = false,
     opts = {
-        stiffness = 0.8,
-        trailing_stiffness = 0.6,
-        trailing_exponent = 0,
-        distance_stop_animating = 0.5,
-        hide_target_hack = false,
         legacy_computing_symbols_support = true,
     },
 }
