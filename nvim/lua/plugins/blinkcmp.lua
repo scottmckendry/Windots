29ee31eea95ba2e5057ce3fa84297f956ea0ead4
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-12-23 21:00:02 +1300

    disable blink.cmp for certain buffers :wheelchair:

--- nvim/lua/plugins/blinkcmp.lua
+++ nvim/lua/plugins/blinkcmp.lua
@@ -3,12 +3,26 @@ return {
     "saghen/blink.cmp",
     lazy = false,
     dependencies = "rafamadriz/friendly-snippets",
-    version = "v0.*",
+    -- TODO: re-enable version releases when auto_show supports functions. Until then, manually build from latest commit.
+    -- version = "v0.*",
+    build = "cargo build --release",
     config = function()
+        local is_enabled = function()
+            local disabled_ft = {
+                "TelescopePrompt",
+                "grug-far",
+            }
+            return not vim.tbl_contains(disabled_ft, vim.bo.filetype)
+                and vim.b.completion ~= false
+                and vim.bo.buftype ~= "prompt"
+        end
+
         require("blink.cmp").setup({
+            enabled = is_enabled,
             completion = {
                 menu = {
                     scrollbar = false,
+                    auto_show = is_enabled,
                     border = {
                         { "󱐋", "WarningMsg" },
                         "─",
