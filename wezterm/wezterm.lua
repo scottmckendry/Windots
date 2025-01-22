bbe78219826f4b6b35b9bead75d7a54ba0be9a89
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-01-22 22:12:58 +0000

    enable anyrun in plasma [skip ci] Automated sync from scottmckendry/nix

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,9 +7,6 @@ local host_os = require("util").get_os()
 
 -- Font
 local emoji_font = "Segoe UI Emoji"
-if host_os == "linux" then
-    emoji_font = "Noto Color Emoji"
-end
 config.font = wezterm.font_with_fallback({
     {
         family = "JetBrainsMono Nerd Font",
@@ -24,17 +21,12 @@ config.initial_rows = 45
 config.initial_cols = 180
 config.window_decorations = "RESIZE"
 config.window_background_opacity = opacity
-config.window_background_image = os.getenv("WEZTERM_CONFIG_FILE"):gsub("wezterm.lua", "bg-blurred.png")
+config.window_background_image = (os.getenv("WEZTERM_CONFIG_FILE") or ""):gsub("wezterm.lua", "bg-blurred.png")
 config.window_close_confirmation = "NeverPrompt"
 config.win32_system_backdrop = "Acrylic"
 config.max_fps = 144
 config.animation_fps = 60
 config.cursor_blink_rate = 250
-if host_os == "linux" then
-    config.front_end = "WebGpu"
-    config.window_background_image = os.getenv("HOME") .. "/.config/wezterm/bg-blurred.png"
-    config.window_decorations = nil -- use system decorations
-end
 
 -- Colors
 config.colors = require("cyberdream")
@@ -42,9 +34,6 @@ config.force_reverse_video_cursor = true
 
 -- Shell
 config.default_prog = { "pwsh", "-NoLogo" }
-if host_os == "linux" then
-    config.default_prog = { "zsh" }
-end
 
 -- Tabs
 config.enable_tab_bar = true
@@ -86,5 +75,14 @@ config.keys = {
     -- Remap paste for clipboard history compatibility
     { key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
 }
+
+-- linux overrides
+if host_os == "linux" then
+    emoji_font = "Noto Color Emoji"
+    config.default_prog = { "zsh" }
+    config.front_end = "WebGpu"
+    config.window_background_image = os.getenv("HOME") .. "/.config/wezterm/bg-blurred.png"
+    config.window_decorations = nil -- use system decorations
+end
 
 return config
