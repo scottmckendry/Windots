e2d625da3714c1b5d11cd3bf4ae5cf55d05c5e75
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-01-30 07:53:31 +1300

    update smear-cursor config :gear:

--- nvim/lua/plugins/smear-cursor.lua
+++ nvim/lua/plugins/smear-cursor.lua
@@ -5,8 +5,14 @@ end
 
 return {
     "sphamba/smear-cursor.nvim",
-    lazy = false,
+    event = "VeryLazy",
     opts = {
         legacy_computing_symbols_support = true,
+        min_horizontal_distance_smear = 3,
+        min_vertical_distance_smear = 3,
+        filetypes_disabled = {
+            "snacks_terminal",
+            "lazy",
+        },
     },
 }
