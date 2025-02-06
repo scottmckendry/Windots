cafdecabe84ef479b08a4ed913d521e7722b738f
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-02-06 23:12:32 +0000

    update cyberdream.nvim config to support 5.0 :arrow_up: [skip ci] Automated sync from scottmckendry/nix

--- nvim/lua/plugins/colourscheme.lua
+++ nvim/lua/plugins/colourscheme.lua
@@ -5,21 +5,19 @@ return {
     priority = 1000,
     config = function()
         require("cyberdream").setup({
+            variant = "auto",
             transparent = true,
             italic_comments = true,
             hide_fillchars = true,
             terminal_colors = false,
             cache = true,
-            borderless_telescope = { border = false, style = "flat" },
-            theme = {
-                variant = "auto",
-                overrides = function(c)
-                    return {
-                        CursorLine = { bg = c.bg },
-                        CursorLineNr = { fg = c.magenta },
-                    }
-                end,
-            },
+            borderless_pickers = true,
+            overrides = function(c)
+                return {
+                    CursorLine = { bg = c.bg },
+                    CursorLineNr = { fg = c.magenta },
+                }
+            end,
         })
 
         vim.cmd("colorscheme cyberdream")
