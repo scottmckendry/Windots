721629c66ad3a973fe3efb1fbc5cd1b05a8c9064
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-02-15 18:48:00 +0000

    add zen mode keymap :cloud: [skip ci] Automated sync from scottmckendry/nix

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -127,6 +127,7 @@ map("n", "<leader>w-", "<C-W>s", { desc = "Split window below", remap = true })
 map("n", "<leader>w|", "<C-W>v", { desc = "Split window right", remap = true })
 map("n", "<leader>-", "<C-W>s", { desc = "Split window below", remap = true })
 map("n", "<leader>|", "<C-W>v", { desc = "Split window right", remap = true })
+map("n", "<leader>wz", function() snacks.zen() end, { desc = "Zen mode" })
 
 -- tabs
 map("n", "<leader><tab><tab>", ":tabnew<cr>", { desc = "New Tab" })
