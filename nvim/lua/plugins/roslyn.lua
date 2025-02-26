9b73dfa5c8c06c36149ba1c4fe3ada4275ddb48b
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-02-26 16:47:33 +0000

    update .net dev config :gear: [skip ci] Automated sync from scottmckendry/nix

--- nvim/lua/plugins/roslyn.lua
+++ nvim/lua/plugins/roslyn.lua
@@ -1,18 +1,7 @@
 return {
     "seblj/roslyn.nvim",
     ft = { "cs" },
-    -- TODO: Add build function that handles roslyn install until Mason supports it:
-    -- https://github.com/mason-org/mason-registry/pull/6330
     config = function()
-        local exe = nil
-        if vim.fn.has("unix") == 1 then
-            local os = vim.fn.systemlist("grep ^ID= /etc/os-release | cut -d= -f2")[1]
-            if os == "nixos" then
-                exe = { "Microsoft.CodeAnalysis.LanguageServer" }
-            end
-        end
-        require("roslyn").setup({
-            exe = exe,
-        })
+        require("roslyn").setup({})
     end,
 }
