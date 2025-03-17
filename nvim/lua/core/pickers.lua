1351ca612cdff7802cb92e9b2b8ef244a743bc2e
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-03-17 20:39:04 +0000

    more session picker progress :chart_with_upwards_trend: [skip ci] Automated sync from scottmckendry/nix

--- nvim/lua/core/pickers.lua
+++ nvim/lua/core/pickers.lua
@@ -1,23 +1,51 @@
 -- WIP: don't use!
+-- Currently not linked to any keybindings or commands - execute with `:luafile %` or 'rlf' keybinding with this file open
 
 local picker = require("snacks").picker
 
-local items = {} ---@type snacks.picker.finder.Item[]
-for _, session in ipairs(require("resession").list()) do
-    items[#items + 1] = {
-        text = session,
-        value = session,
-        display_value = #items + 1 .. ". " .. session,
-    }
+local function generate_sessions()
+    local sessions = {}
+    for idx, session in ipairs(require("resession").list()) do
+        sessions[#sessions + 1] = {
+            text = session,
+            value = session,
+            idx = idx,
+            display_value = idx .. ". " .. session,
+        }
+    end
+    return sessions
 end
 
 picker.pick({
     title = "Sessions",
-    items = items,
-    format = "text",
-    layout = "select",
+    finder = generate_sessions,
+    layout = "vscode",
+    format = function(item)
+        local ret = {} ---@type snacks.picker.Highlight[]
+        ret[#ret + 1] = { item.display_value:match("^%d+%."), "Special" }
+        ret[#ret + 1] = { " " }
+        local formatted_name = item.text:gsub("_", "/")
+        ret[#ret + 1] = { formatted_name, "Normal" }
+        return ret
+    end,
     confirm = function(self, item)
         self:close()
-        print("Selected: " .. item.text)
+        require("resession").load(item.text)
     end,
+    actions = {
+        delete_session = function(self, item)
+            print("Deleted: " .. item.text)
+            require("resession").delete(item.text)
+            self:find({
+                refresh = true,
+            })
+        end,
+    },
+    win = {
+        input = {
+            keys = {
+                ["<C-d>"] = { "delete_session", mode = { "n", "i" }, desc = "Delete session" },
+            },
+        },
+    },
 })
