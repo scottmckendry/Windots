9fbfc430616f70b46b13c9430ef84820c821482f
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-07-29 02:55:52 +0000

    hardcode ps_es mason path :adhesive_bandage: [skip ci] Automated sync from scottmckendry/nix

--- nvim/lsp/powershell_es.lua
+++ nvim/lsp/powershell_es.lua
@@ -1,7 +1,7 @@
 return {
     cmd = function(dispatchers)
         local temp_path = vim.fn.stdpath("cache")
-        local bundle_path = require("mason-registry").get_package("powershell-editor-services"):get_install_path()
+        local bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services"
         local shell = "pwsh"
         local command_fmt =
             [[& '%s/PowerShellEditorServices/Start-EditorServices.ps1' -BundledModulesPath '%s' -LogPath '%s/powershell_es.log' -SessionDetailsPath '%s/powershell_es.session.json' -FeatureFlags @() -AdditionalModules @() -HostName nvim -HostProfileId 0 -HostVersion 1.0.0 -Stdio -LogLevel Normal]]
