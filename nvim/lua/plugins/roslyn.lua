0ff4424fab3168e370ad8bd27e889a6a18dda5b1
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-09-21 02:35:34 +0000

    add rzls, remove redundant docker lsps :knife: [skip ci] Automated sync from scottmckendry/nix

--- nvim/lua/plugins/roslyn.lua
+++ nvim/lua/plugins/roslyn.lua
@@ -1,6 +1,7 @@
 return {
-    "seblj/roslyn.nvim",
+    "seblyng/roslyn.nvim",
     ft = { "cs", "razor" },
+    dependencies = { "tris203/rzls.nvim", config = true },
     init = function()
         vim.filetype.add({
             extension = {
@@ -10,6 +11,51 @@ return {
         })
     end,
     config = function()
-        require("roslyn").setup({})
+        require("roslyn").setup({ lock_target = true })
+
+        local function build_cmd()
+            local log_dir = vim.fs.dirname(vim.lsp.get_log_path())
+            local cmd = {
+                "roslyn",
+                "--stdio",
+                "--logLevel=Information",
+                "--extensionLogDirectory=" .. log_dir,
+            }
+
+            local rzls_libexec = vim.fn.stdpath("data") .. "/mason/packages/rzls/libexec"
+            table.insert(
+                cmd,
+                "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_libexec, "Microsoft.CodeAnalysis.Razor.Compiler.dll")
+            )
+            table.insert(
+                cmd,
+                "--razorDesignTimePath="
+                    .. vim.fs.joinpath(rzls_libexec, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets")
+            )
+            table.insert(cmd, "--extension")
+            table.insert(
+                cmd,
+                vim.fs.joinpath(rzls_libexec, "RazorExtension", "Microsoft.VisualStudioCode.RazorExtension.dll")
+            )
+
+            return cmd
+        end
+
+        local handlers = (function()
+            local ok, h = pcall(require, "rzls.roslyn_handlers")
+            return ok and h or nil
+        end)()
+
+        vim.lsp.config("roslyn", {
+            cmd = build_cmd(),
+            handlers = handlers,
+            settings = {
+                ["csharp|code_lens"] = {
+                    dotnet_enable_references_code_lens = true,
+                },
+            },
+        })
+
+        vim.lsp.enable("roslyn")
     end,
 }
