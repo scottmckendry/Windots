56a41ac1d2d096dee51a68e311b3e715412460f5
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2026-01-01 00:49:25 +0000

    remove duped navic config [skip ci] Automated sync from scottmckendry/nix

--- nvim/lua/plugins/navic.lua
+++ nvim/lua/plugins/navic.lua
@@ -1,4 +1,16 @@
 return {
-    "SmiteshP/nvim-navic",
+    "smiteshp/nvim-navic",
     event = { "BufReadPre", "BufNewFile" },
+    opts = {
+        lsp = {
+            auto_attach = true,
+            -- priority order for attaching LSP servers
+            -- to the current buffer
+            preference = {
+                "html",
+                "templ",
+            },
+        },
+        separator = " 󰁔 ",
+    },
 }
